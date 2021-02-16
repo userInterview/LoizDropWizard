cd81cd61792b85e6927130e2c601c408a43309bd
Loizani <Loizani@aol.com>
2021-02-16 15:23:43 +0100

    tout est ko - en cours de resolution

---
+++ listerJar.sh
@@ -0,0 +1,2 @@
+mvn dependency:build-classpath -Dmdep.pathSeparator="
+" -Dmdep.prefix='' -Dmdep.fileSeparator="" -Dmdep.outputFile=listeDesJars.txt
--- src/main/java/com/dropwizard/application/MainApplication.java
+++ src/main/java/com/dropwizard/application/MainApplication.java
@@ -1,26 +1,24 @@
 package com.dropwizard.application;
 
-
 import com.dropwizard.introduction.configuration.BasicConfiguration;
+import com.dropwizard.introduction.dao.guicemodules.LoizInjectGuiceModule;
 import com.dropwizard.introduction.repositories.BrandDAO;
 import com.dropwizard.introduction.resource.BrandResource;
-
+import com.google.inject.Injector;
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
-import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import ru.vyarus.dropwizard.guice.GuiceBundle;
-import ru.vyarus.guicey.gsp.ServerPagesBundle;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.eclipse.jetty.server.session.SessionHandler;
 import org.skife.jdbi.v2.DBI;
 
 public class MainApplication extends Application<BasicConfiguration> {
 
+	Injector injector;
+	private GuiceBundle<BasicConfiguration> guiceBundle;
+	
     public static void main(final String[] args) throws Exception {
     	System.out.println("dans le main");
     	new MainApplication().run(args);
@@ -33,8 +31,14 @@ public class MainApplication extends Application<BasicConfiguration> {
     
     @Override
     public void initialize(final Bootstrap<BasicConfiguration> bootstrap) {
-    }
+        guiceBundle = GuiceBundle.<BasicConfiguration>newBuilder()
+                .addModule(new LoizInjectGuiceModule())
+                .setConfigClass(BasicConfiguration.class)
+                .build();
+        bootstrap.addBundle(guiceBundle);
+    }    
 
+    
   @Override
     public void run(final BasicConfiguration basicConfiguration, final Environment environment) { 
         final DBIFactory factory = new DBIFactory();        
@@ -42,7 +46,6 @@ public class MainApplication extends Application<BasicConfiguration> {
 
         final BrandDAO brandDAO = jdbi.onDemand(BrandDAO.class);
         final BrandResource personResource = new BrandResource(brandDAO);
-
         environment.jersey().register(personResource);
     	
     	
